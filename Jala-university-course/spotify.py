791bfb0e889dce8551d2ee9e3be0a9a434c7d69c
jowend <joaquinvcestudio@gmail.com>
2023-09-26 20:47:24 -0300

    spotify

---
+++ Jala-university-course/spotify.py
@@ -0,0 +1,47 @@
+class Spotify:
+
+  def __init__(self):
+
+    self.artists = {}
+
+ 
+
+  def add_artist_with_songs(self):
+
+    pass
+
+ 
+
+  def show_all_artists_with_songs(self):
+
+    pass
+
+ 
+
+  def show_songs_from_single_artist(self):
+
+    pass
+
+ 
+
+  def add_song_to_artist(self):
+
+    pass
+
+ 
+
+  def remove_artist(self):
+
+    pass
+
+ 
+
+ 
+
+class Song:
+
+  def __init__(self, name, genre):
+
+    self.name = name
+
+    self.genre = genre
